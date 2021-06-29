25f0ae9dee3f33eff0d3a14a63a95114a2fb4541
Garci4 <juliangarcia.edi@gmail.com>
2021-06-29 15:09:39 -0300

    :tada: aniadida simulacion

--- simulacion.py
+++ simulacion.py
@@ -1,5 +1,14 @@
 from modelo import Camion, Balanza, FabricaTextil
 
+import matplotlib.pyplot as pyplot
+import seaborn as sns
+import pandas as pd
+import numpy as numpy
+#Distribuciones
+from scipy.stats import uniform
+from scipy.stats import norm
+from scipy.stats import expon
+
 class Evento:
   camion = None
   #valor del reloj cuando se crea + la demora obtenida de hacer el evento
@@ -31,6 +40,26 @@ class Simulacion:
     self.fabricaTextil = FabricaTextil(nro_camiones)
     self.reloj = 0
 
+  def calcular_demora_carga_camion(self, camion):
+    if camion.tipo == 1:
+      return 23
+    if camion.tipo == 2:
+      return 20
+    if camion.tipo == 3:
+      return 28
+    if camion.tipo == 4:
+      return 35 
+
+  def calcular_pesaje_segun_tipo_camion(self, camion):
+    if camion.tipo == 1:
+      return norm.rvs(loc=32, scale=6.2)
+    if camion.tipo == 2:
+      return norm.rvs(loc=27.5, scale=4.5)
+    if camion.tipo == 3:
+      return norm.rvs(loc=40, scale=2.3)
+    if camion.tipo == 4:
+      return norm.rvs(loc=49, scale=1.4)
+
   #definimos cada evento
   def inicio_simulacion(self, camiones):
     #por cada camion, generar un evento que contemple el tiempo de carga de c/camion, mas el tiempo de pesaje en barraca
@@ -39,6 +68,10 @@ class Simulacion:
   def inicio_carga_camion_en_barraca(self, camiones, eventos):
     #por cada camion
     #obtener el tiempo que demora cargar el camion
+    for c in range(camiones):
+      c.peso = self.calcular_pesaje_segun_tipo_camion(c)
+
+
 
   def simular(self):
     fabrica_textil = FabricaTextil(5)
@@ -54,7 +87,7 @@ class Simulacion:
           if evento.tipo == "":
             #recalcular cual es el evento futuro
             cuando ocurre = reloj + tiempo
-            #meter en eventops_futuros y reordenar por cuando_ocurre
+            #meter en eventos_futuros y reordenar por cuando_ocurre
           if evento.tipo == "":
           if evento.tipo == "":
           if evento.tipo == "":
